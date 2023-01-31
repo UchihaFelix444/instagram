62358ae1a0838ee959b5ef7dbcdfadc13ece89f0
felix.ar <felix.ar@zohocorp.com>
2023-01-31 08:35:02 +0530

    Firebase configurations

---
+++ lib/utils/web_firestore_config.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const apiKey = "AIzaSyCOq2UB6N-_pS7VueEK0APdILETJUVj0BE";
+const authDomain = "fluttergram-35c01.firebaseapp.com";
+const projectId = "fluttergram-35c01";
+const storageBucket = "fluttergram-35c01.appspot.com";
+const messagingSenderId = "907696534035";
+const appId = "1:907696534035:web:470adfc1cf5d36c5fa45e6";
