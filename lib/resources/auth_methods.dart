d0ecc15e317fbc3991e630caf8aee79edb7ea6f9
felix.ar <felix.ar@zohocorp.com>
2023-02-10 10:13:58 +0530

    Profile pic selection

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -3,6 +3,7 @@ import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:instagram/resources/storage_methods.dart';
 
 class AuthMethods
 {
@@ -20,11 +21,14 @@ class AuthMethods
     String res = "Some error occured";
     try
     {
-       if(!(email.isEmpty || password.isEmpty || username.isEmpty || bio.isEmpty || file != null))
+       if(!(email.isEmpty || password.isEmpty || username.isEmpty || bio.isEmpty))
          {
               UserCredential userCredential = await firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
 
               print(userCredential.user!.uid);
+
+              String photoUrl = await StorageMethods().uploadImageToStorage('profilePics', file, false);
+
 
               await firebaseFirestore.collection("users").doc(userCredential.user!.uid).set({
                     "username" : username,
@@ -34,6 +38,7 @@ class AuthMethods
                     "bio"      : bio,
                     "followers": [],
                     "following": [],
+                    "photoUrl" : photoUrl
               });
               res = "success";
          }
